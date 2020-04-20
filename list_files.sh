0c12e3184046e807165e6c943c54ae873d04f29e
Jad Baz <jadbaz@gmail.com>
2020-04-20 11:51:54 +0000

    No longer mounting zabbix-export as alpine workdir seeing that it causes the parent dir to change group to root

--- list_files.sh
+++ list_files.sh
@@ -1,10 +1,10 @@
-CMD="docker run -it --rm -v $(docker volume ls | grep zabbix-export | awk '{print $2}'):/zabbix-export:ro -w /zabbix-export alpine"
+CMD="docker run -it --rm -v $(docker volume ls | grep zabbix-export | awk '{print $2}'):/zabbix-export:ro alpine"
 
 echo $CMD
 
 echo "Here are your files"
-$CMD ls -l
+$CMD ls -l /zabbix-export
 
 echo
 echo "And here is the head of history-history-syncer-1.ndjson"
-$CMD head history-history-syncer-1.ndjson 
+$CMD head /zabbix-export/history-history-syncer-1.ndjson 
