56e4513f6af1895b8def34694acf9078abd16b5d
Jad Baz <jadbaz@gmail.com>
2020-04-20 09:44:45 +0000

    Added list files and head examples

--- list_files.sh
+++ list_files.sh
@@ -1,1 +1,10 @@
-docker run -it --rm -v $(docker volume ls | grep zabbix-export | awk '{print $2}'):/zabbix-export:ro -w /zabbix-export alpine ls -l
+CMD="docker run -it --rm -v $(docker volume ls | grep zabbix-export | awk '{print $2}'):/zabbix-export:ro -w /zabbix-export alpine"
+
+echo $CMD
+
+echo "Here are your files"
+$CMD ls -l
+
+echo
+echo "And here is the head of history-history-syncer-1.ndjson"
+$CMD head history-history-syncer-1.ndjson 
